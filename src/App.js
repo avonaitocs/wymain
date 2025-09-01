ac5efdcd821be2c17bfed0645f7cd43886113704
Mike Wyman <mgwyman@gmail.com>
2025-08-31 22:18:47 -0300

    Copywrite

--- src/App.js
+++ src/App.js
@@ -254,7 +254,7 @@ const Portfolio = () => {
       {/* Footer */}
       <div className="border-t border-gray-800 py-8">
         <div className="container mx-auto px-6 text-center text-gray-500">
-          <p>&copy; 2024 Your Name. Crafted with AI and creativity.</p>
+          <p>&copy; 2025 WYMaiN</p>
         </div>
       </div>
 
